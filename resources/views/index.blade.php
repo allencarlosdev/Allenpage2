14c16de47dfcc448191d64ef8f5dc9c5a0f6e1e0
CarlosAllen93 <allencarlosdev@gmail.com>
2022-02-17 23:02:15 -0400

    Adding the header

---
+++ resources/views/index.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Valorant blog</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/resetAllen.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+                <div class="header-logo">
+                    <a href="https://playvalorant.com/en-us/">
+                        <img src="{{ asset('img/logo.svg') }}" alt="" class="header-logo__img">
+                        <img src="{{ asset('img/logo-2.svg') }}" alt="" class="header-logo__img header-logo__img--2">
+                    </a>
+                </div>
+                <div class="bar"></div>
+                <div class="menu">
+                    <nav>
+                        <ul class="menu-header">
+                            <li class="menu-header__item"><a href="#home" class="">Home</a></li>
+                            <li class="menu-header__item"><a href="#patch-notes" class="">Patch Notes</a></li>
+                            <li class="menu-header__item"><a href="#highlights" class="">Highlights</a></li>
+                            <li class="menu-header__item"><a href="#" class="">About</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="header-contact">
+                    <span class="header-contact__icon"><i class="fas fa-bars"></i></span>
+                    <a class="header-contact__btn" href="https://github.com/CarlosAllen93" target="_blank">Contact</a>
+                </div>
+        </header>
+        <main class="main">
+            <section class="home">
+                HOME
+            </section>
+            <section class="patchnotes">
+                PATCH NOTES
+            </section>
+
+            <section class="highlights">
+                HIGHLIGHTS
+            </section>
+        </main>
+        <footer class="footer">
+            FOOTER
+        </footer>
+    </div>
+</body>
+</html>
